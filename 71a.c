696dc6ff75b5d5aeb27f9d763f688028af9f184d
kevinvux0 <149225644+kevinvux0@users.noreply.github.com>
2023-11-10 03:48:04 +0000

    Solve 71A - Way Too Long Words

---
+++ 71a.c
@@ -0,0 +1,34 @@
+// strings
+
+#include <stdio.h>
+
+int mstrlen(char *b)
+{
+    int len = 0;
+    for (char *p = b; *p; ++p, ++len)
+        ;
+    return len;
+}
+
+int main(void)
+{
+    int num;
+    scanf("%d", &num);
+
+    for (int i = 0; i < num; ++i)
+    {
+        char buffer[101];
+        scanf("%100s", buffer);
+        int len = mstrlen(buffer);
+
+        if (len <= 10)
+        {
+            printf("%s\n", buffer);
+        }
+        else
+        {
+            printf("%c%d%c\n", buffer[0], len - 2, buffer[len - 1]);
+        }
+    }
+    return 0;
+}
